dc29bd7fd37e988195bd9411f7722e2d311a139c
Kurrgat <mrcykj@gmail.com>
2024-04-07 23:11:02 +0300

    Task 0: Write a Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,38 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
 
-set -e  # Exit on error
+apt-get update
+apt-get install -y nginx
 
-echo "Updating packages..."
-sudo apt-get update
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-echo "Installing Nginx..."
-sudo apt-get -y install nginx
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-echo "Allowing Nginx through firewall..."
-sudo ufw allow 'Nginx HTTP'
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-echo "Creating necessary directories..."
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-echo "Populating index.html with content..."
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-echo "Creating or recreating symbolic link..."
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-echo "Setting ownership of /data/..."
-sudo chown -R ubuntu:ubuntu /data/
-
-echo "Updating Nginx configuration..."
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
-echo "Restarting Nginx..."
-sudo service nginx restart
-
-echo "Web server setup completed successfully."
+service nginx restart
